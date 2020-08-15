459e1d486b9178ac2ff99a9ebae137b3149a9bf9
lacho-georgiev <63149352+lacho-georgiev@users.noreply.github.com>
2020-08-15 18:48:02 +0300

    Update Neural_test.m
    Matrix definition of the problem

--- Neural_test.m
+++ Neural_test.m
@@ -1,17 +1,21 @@
-w1 = rand(6, 25);
-b1 = rand(6, 1);
-w2 = rand(2, 6);
-b2 = rand(2, 1);
+N = 25;
+M = 6;
+K = 2;
+
+w1 = rand(M, N);
+b1 = rand(M, 1);
+w2 = rand(K, M);
+b2 = rand(K, 1);
 training_data;
 n = 10000;
-m = 6;
+m = M;
 ceca = zeros(n, 1);
-for q = 1: n
-    avg_w1 = zeros(6, 25);
-    avg_b1 = zeros(6, 1);
-    avg_w2 = zeros(2, 6);
-    avg_b2 = zeros(2, 1);
-    for p = 1 : m
+for t = 1: n
+    avg_w1 = zeros(M, N);
+    avg_b1 = zeros(M, 1);
+    avg_w2 = zeros(K, M);
+    avg_b2 = zeros(K, 1);
+    for batch = 1 : m
         idx = randi(16);
         a0 = ex{idx};
         y = an{idx};
@@ -23,16 +27,11 @@ for q = 1: n
         dC_dw2 = sum(a1*(sigma1_deriv(z2).*2.*(a2-y))')';
         dC_da2 = 2*(a2 - y);
         dC_db2 = sigma1_deriv(z2).*2.*(a2-y);
-        dC_da1 = zeros(6, 1);
-        for j = 1:2
-           dC_da1 = dC_da1 + (w2(j, :).*sigma1_deriv(z2(j)).*dC_da2(j))';
-        end
-        dC_dw1 = zeros(6, 25);
-        for k = 1:25
-            for j = 1:6
-                dC_dw1(j, k) = a0(k)*sigma1_deriv(z1(j))*dC_da1(j);
-            end
-        end
+        dC_da1 = zeros(M, 1);
+        dC_da1_K = repmat(dC_da1, 1 , K)'; 
+        dC_da1_K = dC_da1_K + (w2.*sigma1_deriv(z2).*dC_da2);
+        dC_da1 = sum(dC_da1_K)';
+        dC_dw1 = (a0.*(sigma1_deriv(z1)'.*dC_da1'))';
         dC_db1 = sigma1_deriv(z1).*dC_da1;
         Y = 0.1;
         w1 = w1 - Y * dC_dw1;
@@ -45,7 +44,7 @@ for q = 1: n
         avg_b2 = avg_b2 + b2;
     end
     c = sum ((a2 - y).^2);
-    ceca(q) = c;
+    ceca(t) = c;
     w1 = avg_w1 ./ m;
     b1 = avg_b1 ./ m;
     w2 = avg_w2 ./ m;
@@ -53,4 +52,3 @@ for q = 1: n
 end
 plot(ceca)
 ylim([0, 1])
-%naural_run(a0, w1, w2, b1, b2);
