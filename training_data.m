412dd44e83fa5bfdbbdd999fef075645f4b9fbca
lachi-georgiev <lachezar.v.georgiev@gmail.com>
2020-12-06 20:37:46 +0200

    training_data was forgotten

---
+++ training_data.m
@@ -0,0 +1,13 @@
+n_ex = 60000;
+ex = cell(n_ex, 1);
+an = cell(n_ex, 1);
+
+images = loadMNISTImages('./data/train-images.idx3-ubyte');
+labels = loadMNISTLabels('./data/train-labels.idx1-ubyte');
+
+for i = 1 : 60000
+	an{i} = [0 0 0 0 0 0 0 0 0 0];
+	ex{i} = images(:, i);
+	an{i}(labels(i) + 1) = 1; % because labels(i) can be 0
+    an{i} = an{i}';
+end
